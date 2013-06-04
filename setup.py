506ac43ea5b8abc31dae2a0882b95f52946dcc9b
Jared Forsyth <jared@jaredforsyth.com>
2013-06-03 18:33:49 -0600

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='vim-debug',
     maintainer='Jared Forsyth',
     maintainer_email='jared@jaredforsyth.com',
-    version='1.5.1',
+    version='1.5.2',
     url='http://jaredforsyth.com/projects/vim-debug/',
     packages=['vim_debug'],
     description='a plugin for vim that creates an integrated debugging environment',
